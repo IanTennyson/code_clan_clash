0c6a25b462df0c7894e7a5953f70357270484316
Ian <iantennyson@gmail.com>
2017-06-16 21:55:53 +0100

    added logic to char array all working

--- code_clan_clash_react/client/src/game/GameBox.jsx
+++ code_clan_clash_react/client/src/game/GameBox.jsx
@@ -8,32 +8,57 @@ class GameBox extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      allWords: ["Ruby", "Java", "JavaScript", "Python"],
+      wordsArray: ["Ruby", "Java", "JavaScript", "Python"],
       charArray: [],
-      currentWord: null,
+      currentWord: "Ruby",
       currentWordIndex: 0,
       charLetterIndex: 0,
       oldUserWords: [],
-      usersCurrentWordAttempt: "",
+      // usersCurrentWordAttempt: "", think this is redundant because of charArray.join("") 
       gameStarted: false
     }
     this.newUserInput = this.newUserInput.bind(this)
+    this.setCurrentWord = this.setCurrentWord.bind(this)
+    this.addToCharArray = this.addToCharArray.bind(this)
+    this.finishCharArray = this.finishCharArray.bind(this)
+
+  }
+
+//Will need to find a way to call this before the game starts
+//For now I will hard code the currentWord to Ruby
+  prepareGame(){
+    this.setState({currentWord: this.state.wordsArray[this.state.currentWordIndex]})
   }
 
   newUserInput(usersInput){
     console.log("usersInput", usersInput)
     if(usersInput === " "){
       this.state.currentWordIndex++
-      
+      this.setCurrentWord();
+      this.finishCharArray(usersInput);
     }
+  }
 
+  setCurrentWord(){
+    this.setState({currentWord: this.state.wordsArray[this.state.currentWordIndex]})
+    // console.log("CURRENT WORD", this.state.currentWord)
+  }
+
+  addToCharArray(userInput){
+    this.state.charArray.push(usersInput)
+    console.log("NEW CHAR ARRAY",this.state.charArray)
+  }
+
+  finishCharArray(){
+
+    this.state.charArray = []
   }
 
   render(){
     return(
       <div>
         <div className="game-box">
-          <Words words={this.state.allWords}/>
+          <Words words={this.state.wordsArray}/>
           <UserInput callback={this.newUserInput}/>
         </div>
         <div className="progress-bar">
--- code_clan_clash_react/client/src/game/Words.jsx
+++ code_clan_clash_react/client/src/game/Words.jsx
@@ -6,8 +6,6 @@ class Words extends React.Component{
     const word = this.props.words.map((word, index) => {
       return <span key={index}>{word}</span>
     })
-    console.log("WORDS", word[0])
-    console.log("WORDS", this.props.words)
   
   return (
     <div className="mapped-word">
