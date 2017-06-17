8f4260f72329d320f39ec7d49af69ad3b3e68fa5
Ian <iantennyson@gmail.com>
2017-06-17 14:51:05 +0100

    game now dynamically sets up on input box clicked

--- code_clan_clash_react/client/src/game/GameBox.jsx
+++ code_clan_clash_react/client/src/game/GameBox.jsx
@@ -11,36 +11,29 @@ class GameBox extends React.Component {
       //GAME STATE
       wordsArray: ["ruby", "java", "javascript", "python"],
       currentWordIndex: 0,
-      currentWord: "ruby",
-      gameCharArray: ["r", "u", "b", "y"],
+      currentWord: null,
+      gameCharArray: [],
       gameCharLetterIndex: 0,
       gameStarted: false,
       //USER STATE
       userCharArray: [],
-      oldUserWords: [],
+      correctUserWords: [],
+      incorrectUserWords: [],
     }
     this.newUserInput = this.newUserInput.bind(this)
     this.setCurrentWord = this.setCurrentWord.bind(this)
     this.addToCharArray = this.addToCharArray.bind(this)
     this.submitFinishedWord = this.submitFinishedWord.bind(this)
-
+    this.spaceBar = this.spaceBar.bind(this)
   }
 
 //Will need to find a way to call this before the game starts
 //For now I will hard code the currentWord to Ruby
-  prepareGame(){
-    this.setState({currentWord: this.state.wordsArray[this.state.currentWordIndex]})
-  }
 
   newUserInput(usersInput){
     console.log("usersInput", usersInput)
     //USER HAS TYPED A SPACE
-    if(usersInput === " "){
-      this.submitFinishedWord(usersInput);
-      this.state.currentWordIndex++
-      this.setCurrentWord();
-      return;
-    }
+
     //USER HAS CORRECTLY TYPED THE DESIRED LETTER
     if(usersInput === this.state.gameCharArray[this.state.gameCharLetterIndex]){
       console.log(usersInput, " === ", this.state.gameCharArray[this.state.gameCharLetterIndex])
@@ -53,12 +46,17 @@ class GameBox extends React.Component {
       this.state.userCharArray.push(usersInput)
       return;
     }
+  }
 
+  spaceBar(){
+      this.submitFinishedWord();
+      this.state.currentWordIndex++
+      this.setCurrentWord();
+      return;
   }
 
   setCurrentWord(){
     this.state.currentWord = this.state.wordsArray[this.state.currentWordIndex]
-    // console.log("CURRENT WORD", this.state.currentWord)
     this.state.gameCharArray = this.state.currentWord.split("")
   }
 
@@ -72,15 +70,17 @@ class GameBox extends React.Component {
     console.log("User Current Word", usersCompletedWord)
     console.log("Game Current Word",this.state.currentWord)
     if(usersCompletedWord === this.state.currentWord){
+    this.state.correctUserWords.push(usersCompletedWord)
       //NEED TO FIGURE OUT HOW TO SET THE CLASS OF THE SPAN!
       //SET SPAN CLASS TO CORRECT
       console.log("CORRECT!")
     }else{
+    this.state.incorrectUserWords.push(usersCompletedWord)
+
       //SET SPAN CLASS TO INCORRECT
       console.log("WRONG!")
     }
     this.state.gameCharLetterIndex = 0
-    this.state.oldUserWords.push(usersCompletedWord)
     this.state.userCharArray = []
   }
 
@@ -89,7 +89,10 @@ class GameBox extends React.Component {
       <div>
         <div className="game-box">
           <Words words={this.state.wordsArray}/>
-          <UserInput callback={this.newUserInput}/>
+          <UserInput 
+          spaceBar={this.spaceBar} 
+          letterInput={this.newUserInput} 
+          prepareGame={this.setCurrentWord} />
         </div>
         <div className="progress-bar">
           
--- code_clan_clash_react/client/src/game/UserInput.jsx
+++ code_clan_clash_react/client/src/game/UserInput.jsx
@@ -4,13 +4,21 @@ class UserInput extends React.Component {
 
   render(){
     return(
-      <input className="user-input-box" placeholder="Type to begin" onKeyUp={this.triggerInput.bind(this)}></input>
+      <input 
+      className="user-input-box" 
+      placeholder="Type to begin" 
+      onKeyUp={this.triggerInput.bind(this)}
+      onClick={this.props.prepareGame}></input>
     )
   }
 
   triggerInput(event){
     const newChar = event.target.value.slice(-1)
-    this.props.callback(newChar)
+    if(newChar === " "){
+      this.props.spaceBar()
+    }else{
+    this.props.letterInput(newChar)
+    }
   }
 
 }
