bdc590f8365299ac91bdda27c49e802df9ca3258
Ian <iantennyson@gmail.com>
2017-06-19 21:38:15 +0100

    long day but very happy that my classnames are now set by my game logic meaning that they are styled appropriately woooohooo

--- code_clan_clash_react/client/src/game/GameBox.jsx
+++ code_clan_clash_react/client/src/game/GameBox.jsx
@@ -9,26 +9,43 @@ class GameBox extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      wordsArray: ["now", "now", "now", "now", "now"],
+      // wordsArray: ["now", "now", "now", "now", "now"],
+      wordsArrayObj: [
+      {value: "first", status: "nextword"},
+      {value: "new", status: "nextword"},
+      {value: "new", status: "nextword"},
+      {value: "new", status: "nextword"},
+      {value: "new", status: "nextword"},
+      ],
+
+      // [{value: "new", status: "currentword"},]
+
+
       currentWordIndex: 0,
       currentWord: null,
-      gameCharLetterIndex: 0,
       gameStarted: false,
       numberOfUserInputs: 0,
       indexsOfUncorrectedWords: [],
       grossWPM: 0,
       netWPM: 0,
-      startTime: 0.00
+      startTime: 0.00,
+      gameOver: false,
+      
     }
-    this.submitFinishedWord = this.submitFinishedWord.bind(this)
-    this.setCurrentWord = this.setCurrentWord.bind(this)
-    this.calculateWPM = this.calculateWPM.bind(this)
-    this.backspace = this.backspace.bind(this)
-    this.startGame = this.startGame.bind(this)
-    this.spaceBar = this.spaceBar.bind(this)
-    this.stopGame = this.stopGame.bind(this)
-    this.beginGame = this.beginGame.bind(this)
+    this.submitFinishedWord = this.submitFinishedWord.bind(this);
+    this.setCurrentWord = this.setCurrentWord.bind(this);
+    this.calculateWPM = this.calculateWPM.bind(this);
+    this.backspace = this.backspace.bind(this);
+    this.startGame = this.startGame.bind(this);
+    this.beginGame = this.beginGame.bind(this);
+    this.spaceBar = this.spaceBar.bind(this);
+    this.stopGame = this.stopGame.bind(this);
+    this.updateClassName = this.updateClassName.bind(this);
+    this.componentWillUpdate = this.componentWillUpdate.bind(this)
   }
+
+
+
 
   beginGame(){
     this.setState({gameStarted: true}) 
@@ -43,16 +60,16 @@ class GameBox extends React.Component {
     const timer = setInterval(() => {
       this.calculateWPM()
       counter++
-      if(counter === 12){
+      if(counter === 120){
         clearInterval(timer)
         this.stopGame()
       }
-    }, 5000)
+    }, 500)
   }
 
   stopGame(){
     console.log("GAME OVER!")
-    this.setState({gameStarted: false})
+    this.setState({gameStarted: false, gameOver: true})
     //Need to reset the progress bar
     //need to reset the "time" currently if the game is restarted it will continue from 60 seconds
   }
@@ -62,48 +79,34 @@ class GameBox extends React.Component {
 
     const allUserInput = this.state.numberOfUserInputs
     let uncorrectedErrors = this.state.indexsOfUncorrectedWords.length
-    const addTime = 0.05
+    const addTime = 0.01
     const time = (this.state.startTime += addTime).toFixed(2)
-    console.log("Time Passed: ", time)
 
     const grossWPM = (allUserInput / 5)
     const predictedGrossWPM = grossWPM / time
 
-    let netWPM = (grossWPM - uncorrectedErrors) / time
+    let netWPM = (grossWPM - uncorrectedErrors) / 1
     const roundedGrossWPM = parseInt(predictedGrossWPM)
     const roundedNetWPM = parseInt(netWPM)
 
-
-
-    //Why is this re-running my gameStarted state?!
-    //Do I need to use ShoudComponentUpdate?
     this.setState({ grossWPM: roundedGrossWPM, netWPM: roundedNetWPM })
   }
 
   spaceBar(userSubmittedWord){
-      console.log("userSubmittedWord Length",userSubmittedWord.length)
       this.state.numberOfUserInputs += userSubmittedWord.length
       this.submitFinishedWord(userSubmittedWord);
       this.state.currentWordIndex++
       this.setCurrentWord();
       return;
   }
-// 1. If user succesfully completes a word, presses space and presses backpace, space it comes back as a correct. Need to test if this will mess with the WPM score.
+
   backspace(allUserText){
     console.log("allUserText",allUserText)
 
     if(allUserText.slice(-1) === " "){
+      this.updateClassName('nextword')
       this.state.currentWordIndex--;
       this.setCurrentWord();
-      console.log("CURRENT WORD: ", this.state.currentWord)
-      this.state.gameCharLetterIndex = this.state.currentWord.length
-      console.log("gameCharLetterIndex", this.state.gameCharLetterIndex)
-      
-      // console.log("currentWordIndex",this.state.currentWordIndex)
-      // console.log("ALL INCORRECT WORDS INDEXS",this.state.indexsOfUncorrectedWords)
-      // console.log("MOST RECENT INCORRECT WORD INDEX",this.state.indexsOfUncorrectedWords.slice(-1))
-// console.log(this.state.indexsOfUncorrectedWords.slice(-1), "===" ,this.state.currentWordIndex )
-
 
       if(this.state.indexsOfUncorrectedWords.slice(-1).pop() === this.state.currentWordIndex){
         console.log("index of wrong word is equal to currentWordIndex")
@@ -111,49 +114,58 @@ class GameBox extends React.Component {
       }
       return;
     };
-    this.state.gameCharLetterIndex--
+
   }
 
   setCurrentWord(){
-    this.state.currentWord = this.state.wordsArray[this.state.currentWordIndex]
+    this.state.currentWord = Object.values(this.state.wordsArrayObj[this.state.currentWordIndex])[0]
+    this.updateClassName('currentword')
+  }
+
+  updateClassName(newClassName){
+    this.state.wordsArrayObj[this.state.currentWordIndex]['status'] = newClassName
+  }
+
+  componentWillUpdate(){
   }
 
   submitFinishedWord(userSubmittedWord){
-      console.log("CURRENT WORD INDEX", this.state.currentWordIndex)
-
-    console.log(userSubmittedWord, "===", this.state.currentWord)
-    if(userSubmittedWord === this.state.currentWord){
-      //SET SPAN CLASS TO CORRECT
+    if(userSubmittedWord.pop() === this.state.currentWord ){
+      this.updateClassName('correct')
       console.log("CORRECT!")
     }else{
-      //SET SPAN CLASS TO INCORRECT
+      this.updateClassName('incorrect')
       console.log("INCORRECT!")
       this.state.indexsOfUncorrectedWords.push(this.state.currentWordIndex)
       console.log("indexsOfUncorrectedWords", this.state.indexsOfUncorrectedWords)
     }
-    // this.state.finishedUserWords.push(userSubmittedWord)
-    this.state.gameCharLetterIndex = 0
   }
 
   render(){
+    const pageDiv = 
+      <div className="master-game-div">
+        <Score grossWPM={this.state.grossWPM} netWPM={this.state.netWPM}/>
+        <Words 
+        words={this.state.wordsArrayObj} 
+        
+        currentWordIndex={this.state.currentWordIndex}
+        indexsOfUncorrectedWords={this.state.indexsOfUncorrectedWords}/>
+        <UserInput 
+        spaceBar={this.spaceBar} 
+        triggerBackspace={this.backspace}
+        letterInput={this.newUserInput} 
+        prepareGame={this.setCurrentWord}
+        startTest={this.beginGame} 
+        hasGameStarted={this.state.gameStarted}
+        />
+        <ProgressBar hasGameStarted={this.state.gameStarted} />
+        <ReloadPage />
+      </div>
+
+
     return(
       <div>
-        <div className="game-box">
-          <Score grossWPM={this.state.grossWPM} netWPM={this.state.netWPM}/>
-          <Words words={this.state.wordsArray}/>
-          <UserInput 
-          spaceBar={this.spaceBar} 
-          triggerBackspace={this.backspace}
-          letterInput={this.newUserInput} 
-          prepareGame={this.setCurrentWord}
-          startTest={this.beginGame} 
-          hasGameStarted={this.state.gameStarted}
-          />
-        </div>
-
-        <ProgressBar hasGameStarted={this.state.gameStarted} />
-
-        <ReloadPage />
+        {pageDiv}
       </div>
     )
   }
--- code_clan_clash_react/client/src/game/Words.jsx
+++ code_clan_clash_react/client/src/game/Words.jsx
@@ -2,10 +2,38 @@ import React from 'react'
 
 class Words extends React.Component{
 
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    // console.log("loaded");
+    // console.log(this.refs);
+    // console.log(Object.keys(this.refs));
+  }
+  // Object.values(this.state.wordsArrayObj[this.state.currentWordIndex])[0]
   render(){
-    const word = this.props.words.map((word, index) => {
-      return <span key={index}>{word}</span>
-    })
+    // console.log("this.props.words", Object.values(this.props.words))
+    const word = Object.values(this.props.words).map((word, index) => {
+      // console.log("value: ", value, "index: ", index)
+      // console.log("Word: ", word)
+      return <span 
+        ref={"wordspan" + index}
+        className={word.status} 
+        key={index}>
+        {word.value}
+        </span>
+    }) 
+
+    // this.props.words.map((word, index) => {
+    //   return <span 
+    //     ref={"wordspan" + index}
+    //     // className={this.assignClass.bind(this)} 
+    //     //onLoad={this.assignClass.bind(this)}
+    //     key={index}>
+    //     {word}
+    //     </span>
+    // })
   
   return (
     <div className="mapped-word">
@@ -14,6 +42,11 @@ class Words extends React.Component{
   );
 
   }
+
+
 }
+
+
+
 
 export default Words;
